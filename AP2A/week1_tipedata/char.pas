fff864df87c42d5a1bf3e5208ef7dc7cfdf04d39
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:28:15 +0700

    Create char.pas

---
+++ AP2A/week1_tipedata/char.pas
@@ -0,0 +1,12 @@
+program variabel_char;
+uses crt;
+//deklarasi variabel char
+var nilai_char : char;
+begin
+	clrscr;
+	//input nilai char dan output nya
+	write('Masukan Nilai char : ');
+	read(nilai_char);
+	writeln('Nilai char ', nilai_char);
+	writeln('@wisnuazfar')
+end.
