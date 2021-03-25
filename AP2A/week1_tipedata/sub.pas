a4c65bfcf3952173352a7ce917c4cec97a053c17
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:29:21 +0700

    Create sub.pas

---
+++ AP2A/week1_tipedata/sub.pas
@@ -0,0 +1,20 @@
+program tipe_subrange;
+//digunakan untuk menggunakan range
+uses crt;
+type
+  satuan= 1..9;
+var
+  a,b: satuan;
+begin
+  clrscr;
+  
+
+  a:= 3;
+  writeln('a: ',a);
+  
+  b:= 7;
+  writeln('b: ',b);
+  writeln('@wisnuazfar');
+  
+  readln;
+end.
