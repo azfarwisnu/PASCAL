9af19323397f9d49dd4ebbd7471888c6c85d05ac
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:28:30 +0700

    Create enumarated.pas

---
+++ AP2A/week1_tipedata/enumarated.pas
@@ -0,0 +1,16 @@
+program tipe_enumerated;
+uses crt;
+type
+  nama_hari= (senin,selasa,rabu,kamis,jumat,sabtu,minggu);
+var
+  a,b: nama_hari;
+begin
+  clrscr;
+  
+  a:= senin;
+  writeln('a: ',a);
+  
+  b:= minggu;
+  writeln('b: ',b);
+  writeln('@wisnuazfar')
+end.
