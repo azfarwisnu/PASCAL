cf9ab5fc172f2c304c95b554109df6f758cf9375
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:27:54 +0700

    Create boolean.pas

---
+++ AP2A/week1_tipedata/boolean.pas
@@ -0,0 +1,11 @@
+program variabel_boolean;
+uses crt;
+//deklarasi variabel boolean
+var nilai_boolean : boolean;
+begin
+	clrscr;
+	//input nilai boolean dan output nya
+	nilai_boolean := something;
+	writeln('nilai boolean ',nilai_boolean);
+	writeln('@wisnuazfar');
+end.
