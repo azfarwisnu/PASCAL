e6f5a79a232a2decb0d4ebadbe38b8c9e6d2bf3b
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:28:43 +0700

    Create real.pas

---
+++ AP2A/week1_tipedata/real.pas
@@ -0,0 +1,12 @@
+program variabel_real;
+uses crt;
+//deklarasi variabel real
+var nilai_real : real;
+begin
+	clrscr;
+	//input nilai real dan output nya
+	write('Masukan Nilai real : ');
+	read(nilai_real);
+	writeln('Nilai real', nilai_real);
+	writeln('@wisnuazfar')
+end.
