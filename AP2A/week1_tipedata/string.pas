d5e55ce7b2b9b0bcbbb0541e05f0188b2053c850
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:29:09 +0700

    Create string.pas

---
+++ AP2A/week1_tipedata/string.pas
@@ -0,0 +1,12 @@
+program variabel_string;
+uses crt;
+//deklarasi variabel string
+var nilai_string : string;
+begin
+	clrscr;
+	//input nilai string dan output nya
+	write('Masukan Nilai string : ');
+	read(nilai_string);
+	writeln('Nilai string ', nilai_string);
+	writeln('@wisnuazfar')
+end.
