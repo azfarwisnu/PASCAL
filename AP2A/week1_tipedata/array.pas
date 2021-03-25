7dad5688cd8220ce005b7a9541ea6cc7107e1a66
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:27:40 +0700

    Create array.pas

---
+++ AP2A/week1_tipedata/array.pas
@@ -0,0 +1,20 @@
+program tipe_array;
+uses crt;
+var
+  nilai1, nilai2, nilai3, nilai4: integer;
+begin
+  clrscr;
+  
+  nilai1:= 23;
+  nilai2:= 13;
+  nilai3:= 98;
+  nilai4:= 106;
+  
+  writeln('nilai1: ',nilai1);
+  writeln('nilai2: ',nilai2);
+  writeln('nilai3: ',nilai3);
+  writeln('nilai4: ',nilai4);
+  writeln('@wisnuazfar');
+  
+  readln;
+end.
