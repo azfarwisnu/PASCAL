82f4d6cd19ebce2e493a84a13fe300cca5c57002
wisnuazfar <47131350+azfarwisnu@users.noreply.github.com>
2021-03-25 18:29:52 +0700

    Create integer.pas

---
+++ AP2A/week1_tipedata/integer.pas
@@ -0,0 +1,12 @@
+program varibael_input_and_output;
+uses crt;
+//deklarasi variabel integer
+var nilai_integer : integer;
+begin
+	clrscr;
+	//input nilai integer dan output nya
+	write('Masukan Nilai Integer : ');
+	read(nilai_integer);
+	writeln('Nilai interger', nilai_integer);
+end.
+
